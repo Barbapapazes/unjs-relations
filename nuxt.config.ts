f9ff750092f909ae795438e274ca9ea63eba067b
barbapapazes <e.soubiran25@gmail.com>
2023-11-11 17:49:40 +0100

    fix: prerender api endpoint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: ['/'],
+      routes: ['/', '/api/packages'],
     },
     routeRules: {
       '/api/packages': {
