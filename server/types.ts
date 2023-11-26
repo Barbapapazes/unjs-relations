0d6dd9656223bcc0e06d70bddff67536b9493d13
Estéban <e.soubiran25@gmail.com>
2023-11-26 19:04:44 +0100

    fix: use npm instead of github (#10)

--- server/types.ts
+++ server/types.ts
@@ -1,3 +1,21 @@
+export interface UnjsPackage {
+  title: string
+  description: string
+  path: string
+  url: string
+  npm?: {
+    name: string
+  }
+}
+
+export interface NpmPackage {
+  name: string
+  description: string
+  version: string
+  dependencies?: string[]
+  devDependencies?: string[]
+}
+
 // https://github.com/unjs/ungh/tree/main#reposownername
 export interface GitHubRepo {
   'id': number
